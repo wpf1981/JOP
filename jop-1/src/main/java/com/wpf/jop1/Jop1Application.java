e6310faddb98cc36082ea1f25dfd01a14cdd38c9
wpf1981 <45836699+wpf1981@users.noreply.github.com>
2018-12-24 08:21:04 +0800

    建立项目

---
+++ jop-1/src/main/java/com/wpf/jop1/Jop1Application.java
@@ -0,0 +1,14 @@
+package com.wpf.jop1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jop1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jop1Application.class, args);
+    }
+
+}
+
